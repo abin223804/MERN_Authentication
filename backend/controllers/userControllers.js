8bd7fc96ff156bef83e65c41c14b4f7c69e341be
Abin s chandran <abinschandran@gmail.com>
2023-10-07 22:52:14 +0530

    user registration wrote and tested successfully

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,12 +1,35 @@
 import asyncHandler from "express-async-handler";
-import User from '../models/userModel.js'
+import User from "../models/userModel.js";
 
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Auth User"});
+
+   res.status(200).json({message: "Auth user"});
+   
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Register User"});
+    
+    const {name, email, password} = req.body;
+    const userExist = await User.findOne({email});
+    if (userExist) {
+        res.status(400);
+        throw new Error("User already Exist");
+    }
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid User data");
+    }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
