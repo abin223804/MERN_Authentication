bb94b2a9a2fa2753ffe840e4ea028b5d05f2beac
Abin s chandran <abinschandran@gmail.com>
2023-10-12 23:06:45 +0530

    all router to login,Register is set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Outlet } from 'react-router-dom';
+import {Container} from 'react-bootstrap'
 import Header from "./components/Header";
+
 
 const App = () => {
     return (
-    <>
-    <Header/>
-    <h1> Mern Auth </h1>
-    </>
-    )
+        <>
+            <Header />
+            <Container className="my-2">
+            <Outlet />
+            </Container>
+            
+        </>
+    );
 };
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css' ;
 import './index.css'
+import Homescreen from './screens/Homescreen.jsx';
+
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path='/' element={<Homescreen/>}></Route>
+
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router}/>
   </React.StrictMode>,
 )
